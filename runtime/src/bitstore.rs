fdeddf8c39a333a3b2aec378fb6b7cceda314a78
Joshy Orndorff <admin@joshyorndorff.com>
2019-06-10 16:58:58 -0400

    Declared one boolean in storage, and wrote a few dispatchable calls.

--- runtime/src/bitstore.rs
+++ runtime/src/bitstore.rs
@@ -13,7 +13,7 @@ use system::ensure_signed;
 
 /// The module's configuration trait.
 pub trait Trait: system::Trait {
-	// TODO: Add other types and constants required configure this module.
+	// TODO: Add other types and constants required configure this module, if any.
 
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -22,10 +22,14 @@ pub trait Trait: system::Trait {
 /// This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item. 
 		// Here we are declaring a StorageValue, `Something` as a Option<u32>
 		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
+		//Something get(something): Option<u32>;
+
+        // The single bit that our module stores
+        Bit get(get_bit) : bool;
+        // creates a function like
+        // fn get_bit(origin) -> bool
 	}
 }
 
@@ -39,18 +43,52 @@ decl_module! {
 		// Just a dummy entry point.
 		// function that can be called by the external world as an extrinsics call
 		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		// pub fn do_something(origin, something: u32) -> Result {
+		// 	// TODO: You only need this if you want to check it was signed.
+		// 	let who = ensure_signed(origin)?;
+        //
+		// 	// TODO: Code to execute when something calls this.
+		// 	// For example: the following line stores the passed in u32 in the storage
+		// 	<Something<T>>::put(something);
+        //
+		// 	// here we are raising the Something event
+		// 	Self::deposit_event(RawEvent::SomethingStored(something, who));
+		// 	Ok(())
+		// }
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			<Something<T>>::put(something);
+        pub fn set(origin) -> Result {
+            // Make sure this call actually came from a person.
+            let _ = ensure_signed(origin)?;
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
-		}
+            // Actually set the bit in storage
+            <Bit<T>>::put(true); //TODO what is that method called.
+
+            // Return Success
+            Ok(())
+        }
+
+        pub fn clear(origin) -> Result {
+            // Make sure this call actually came from a person.
+            let _ = ensure_signed(origin)?;
+
+            // Actually clear the bit in storage
+            <Bit<T>>::put(false);
+
+            // Return Success
+            Ok(())
+        }
+
+        pub fn toggle(origin) -> Result {
+            // Make sure this call actually came from a person.
+            let _ = ensure_signed(origin)?;
+
+            let old : bool = Self:: get_bit(); // I think we get the actual boolean
+            let new = ! old;
+
+            <Bit<T>>::put(new);
+
+            Ok(())
+        }
 	}
 }
 
